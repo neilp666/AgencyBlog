d11ddfb8dcaf25ff2b4bd77d5baed81a7b993723
neil patel <neil@myindianweddingapp.com>
2016-02-27 11:48:29 +0000

    Created Posts Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/about'
+  
+  get 'about' => "pages#about"
+  get 'contact' => "pages#contact"
 
-  get 'pages/contact'
+  get "posts" => "posts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
