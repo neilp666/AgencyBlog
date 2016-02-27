56476d9d18df93d9ce0de99031304203bdef0cd2
neil patel <neil@myindianweddingapp.com>
2016-02-27 14:50:42 +0000

    updated controller for edit & delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   get 'about' => "pages#about", as: :about
   get 'contact' => "pages#contact", as: :contact
 
-  get "/posts" => "posts#index"
+  get "/posts" => "posts#index", as: :posts
   post "/posts" => "posts#create"
 
   get "/post/:id" => "posts#show", as: :post
 
   patch "post/:id" => "posts#update"
-  get "/posts/new" => "posts#new"
+
+  patch "post/:id" => "posts#update"
+  delete "post/:id" => "posts#destroy"
+
+  get "/posts/new" => "posts#new", as: :new_post
 
   get "/post/:id/edit" => "posts#edit", as: :edit_post
 
